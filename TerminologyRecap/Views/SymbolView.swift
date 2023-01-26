d31a219346ec729ff91950ad90ec2d964beab8ab
Justin Hui <jhui@lcs.on.ca>
2023-01-26 09:43:00 -0500

    Removed content view and set up project with new symbol view

---
+++ TerminologyRecap/Views/SymbolView.swift
@@ -0,0 +1,22 @@
+//
+//  SymbolView.swift
+//  TerminologyRecap
+//
+//  Created by Justin Hui on 26/1/2023.
+//
+
+import SwiftUI
+
+struct SymbolView: View {
+    var body: some View {
+        
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+    }
+}
+
+struct SymbolView_Previews: PreviewProvider {
+    static var previews: some View {
+        SymbolView()
+    }
+}
