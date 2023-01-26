244ab29a4301cf19cd4266be83e3cab7eca2fbdf
Justin Hui <jhui@lcs.on.ca>
2023-01-26 09:36:10 -0500

    Initial Commit

---
+++ TerminologyRecap/TerminologyRecapApp.swift
@@ -0,0 +1,17 @@
+//
+//  TerminologyRecapApp.swift
+//  TerminologyRecap
+//
+//  Created by Justin Hui on 26/1/2023.
+//
+
+import SwiftUI
+
+@main
+struct TerminologyRecapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
